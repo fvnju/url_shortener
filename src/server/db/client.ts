8fa9587ce7ed0f15e1beb8fcb1b3820635da33c7
Favour Njoku <favour.njoku@icloud.com>
2025-01-25 22:18:32 +0100

    chore: clean up

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,10 +1,22 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
+import "dotenv/config";
 
-config({ path: ".env" }); // or .env.local
+let uri = "";
+try {
+  uri = process.env.DATABASE_URL!;
+  if (!uri) {
+    throw new Error("no uri");
+  }
+} catch (error) {
+  config({ path: ".env" });
+  uri = process.env.DATABASE_URL!;
+} finally {
+  uri =
+    "postgresql://neondb_owner:npg_LBrNXv3k8FmR@ep-fancy-field-a2lj6g41-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require";
+}
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(uri);
 const db = drizzle({ client: sql });
-
 export default db;
