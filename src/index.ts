1d90f747c489d1a038d04a0751eb4d932dcc2c5c
Favour Njoku <favour.njoku@icloud.com>
2025-01-25 16:10:15 +0100

    fix: serves build static files

--- src/index.ts
+++ src/index.ts
@@ -2,43 +2,11 @@ import { Hono } from "hono";
 import { createShortUrlHandler } from "./server/controller/create";
 import redirectHandler from "./server/controller/redirect";
 import { jsxHydration, renderer } from "./server/controller/jsxRenderer";
-import { serveStatic } from "hono/serve-static";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
-if (import.meta.env.PROD) {
-  app.use(
-    "/assets/index.js",
-    serveStatic({
-      path: "./public/assets/index.js",
-      getContent: async (path) => {
-        const response = await fetch(path);
-        if (response.ok) {
-          console.log("worked");
-          return response;
-        }
-        console.log("not worked");
-        return null;
-      },
-    })
-  );
-
-  app.use(
-    "/assets/index.css",
-    serveStatic({
-      path: "./public/assets/index.css",
-      getContent: async (path) => {
-        const response = await fetch(path);
-        if (response.ok) {
-          console.log("worked");
-          return response;
-        }
-        console.log("not worked");
-        return null;
-      },
-    })
-  );
-}
+app.use("/*", serveStatic({ root: "./public/" }));
 
 const route = app.get("/mk", (c) => {
   console.log("used");
